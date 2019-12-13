b34e07d140e219ff36f0ff2c68538b79e361165e
Juank <juanc.guana@gmail.com>
2019-12-13 08:08:37 -0500

    second commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Aprende a disenar y codificar apps con React</h1>
+    <p>Curso completo acerca de las herramientas y sistemas de diseno. Prototipado y construccion de apps con React and Swift.</p>
+    <Link to="/page-2/">Look at</Link>
+  </div>
+)
+
+export default IndexPage
