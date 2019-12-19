e971e1263c1d20d201fea9862f8650977c800cbe
Juank <juanc.guana@gmail.com>
2019-12-19 09:59:20 -0500

    Estilos basicos con CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Aprende a disenar y codificar apps con React</h1>
-    <p>Curso completo acerca de las herramientas y sistemas de diseno. Prototipado y construccion de apps con React and Swift.</p>
-    <Link to="/page-2/">Look at</Link>
+    <div className="Hero"> 
+      <div className="HeroGroup"> 
+        <h1>Aprende a disenar y codificar apps con React</h1>
+        <p>Curso completo acerca de las herramientas y sistemas de diseno. Prototipado y construccion de apps con React and Swift.</p>
+        <Link to="/page-2/">Ver video</Link>
+      </div>
+    </div>
   </div>
 )
 
