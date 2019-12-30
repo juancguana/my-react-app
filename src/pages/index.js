b2c210d6fe1ce240ddbd215ae27d7c4d433b9efd
Juank <juanc.guana@gmail.com>
2019-12-29 22:06:45 -0500

    Finalizo Interactions and Animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <div className="Hero"> 
       <div className="HeroGroup"> 
-        <h1>Aprende a disenar y codificar apps con React</h1>
+        <h1>Aprende a <br />disenar y codificar apps con React</h1>
         <p>Curso completo acerca de las herramientas y sistemas de diseno. Prototipado y construccion de apps con React and Swift.</p>
         <Link to="/page-2/">Ver video</Link>
       </div>
