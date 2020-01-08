505a63321fa1b449a52fa559a6897b1c292d09a1
Juank <juanc.guana@gmail.com>
2020-01-08 12:53:31 -0500

    Finaliza el tema States and events

--- src/components/header.js
+++ src/components/header.js
@@ -2,17 +2,42 @@ import React from 'react'
 import Link from 'gatsby-link'
 import '../components/Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup"> 
-    <Link to="/"><img src={'../images/logo-designcode.svg'} width="30" /></Link>
-    <Link to="/courses">Cursos</Link>
-    <Link to="/downloads">Descargas</Link>
-    <Link to="/workshops">Talleres</Link>
-    <Link to="/buy"><button>Comprar</button></Link>
-    </div>
-  </div>
+class Header extends React.Component {
+ constructor(props) {
+   super(props)
 
-)
+   this.state = {
+     hasScrolled: false
+   }
+ }
+
+ componentDidMount() {
+   window.addEventListener('scroll', this.handleScroll)
+ }
+
+ handleScroll = (event) => {
+   const scrollTop = window.pageYOffset
+
+   if (scrollTop > 50) {
+     this.setState({hasScrolled: true})
+   } else {
+     this.setState({hasScrolled: false})
+   }
+ }
+ 
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup"> 
+          <Link to="/"><img src={'../images/logo-designcode.svg'} width="30" /></Link>
+          <Link to="/courses">Cursos</Link>
+          <Link to="/downloads">Descargas</Link>
+          <Link to="/workshops">Talleres</Link>
+          <Link to="/buy"><button>Comprar</button></Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
