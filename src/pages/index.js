1e238f0f609eae4f195f10515c96a54de3a372f0
Juank <juanc.guana@gmail.com>
2020-01-12 18:07:12 -0500

    Finaliza el video styled-components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import Card from '../components/Card'
+import Card from '../components/Card';
+import Section from '../components/Section';
+import Wave from '../components/Wave';
+
 
 const IndexPage = () => (
   <div>
     <div className="Hero"> 
       <div className="HeroGroup"> 
-        <h1>Aprende a <br />disenar y codificar apps con React</h1>
+        <h1>Aprende a diseñar<br /> y codificar apps con React</h1>
         <p>Curso completo acerca de las herramientas y sistemas de diseno. Prototipado y construccion de apps con React and Swift.</p>
         <Link to="/page-2/">Ver video</Link>
         <div className="Logos"> 
@@ -17,18 +20,7 @@ const IndexPage = () => (
           <img src={'../images/logo-react.png'} width="50"/>
           <img src={'../images/logo-swift.png'} width="50"/>
         </div>
-        <svg width="100%" height="172" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fill="white">
-          <animate repeatCount="indefinite"
-          fill="freeze" attributeName="d" dur="10s"
-          values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z;
-          M0 87.1596C316 87.1597 444 160 884 52.0001C1324 -55.9999 1320.29 34.966 1538 71.251C1814 117.251 2156 189.252 2560 87.1597V233.161L0 233.161V87.1596Z;
-          M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440 115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z;
-          M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z
-          "
-          />
-          </path>
-        </svg>
+        <Wave />
       </div>
     </div>
     <div className="Cards">
@@ -49,9 +41,22 @@ const IndexPage = () => (
         <Card
           title="React Native para Diseñaddores"
           text="10 secciones" 
-          image={'../images/wallpaper4.jpg'}/>
-        </div>
+          image={'../images/wallpaper4.jpg'}
+
+        />
+      </div>
     </div>
+    <Section
+      image={'../images/wallpaper2.jpg'}
+      logo={'../images/logo-react.png'}
+      title="React para Diseñadores"
+      text="Aprende como construir un moderno sitio 
+      web usando React y la mas eficientes librerias
+      para obtener tu sitio online y en produccion.
+      Familiarizate con componentes. Grid CSS,
+      animaciones, interacciones, datos dinamicos 
+      con contenido y desarrolla tu sitio con Netlifi."
+    />     
   </div>
 )
 
